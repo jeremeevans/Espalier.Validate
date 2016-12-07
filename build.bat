6c1093dac6b65a9b7e71865742546eb67e0354d2
Jereme Evans <jereme@jeremeevans.com>
2016-12-07 09:01:18 -0800

    dotnet restore for test project

--- build.bat
+++ build.bat
@@ -12,10 +12,12 @@ if "%nuget%" == "" (
 
 set nunit=".\NUnit.Console-3.5.0\nunit3-console.exe"
 
+dotnet restore .\Espalier.Validate.Tests\project.json
+dotnet restore .\Espalier.Validate\project.json
+
 dotnet test .\Espalier.Validate.Tests\
 if not "%errorlevel%"=="0" goto failure
 
-dotnet restore .\Espalier.Validate\project.json
 dotnet build .\Espalier.Validate\project.json --configuration Release
 if not "%errorlevel%"=="0" goto failure
 
