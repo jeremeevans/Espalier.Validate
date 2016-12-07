9c6162043a69c3aee38322104e844696213509a0
Jereme Evans <jereme@jeremeevans.com>
2016-12-07 08:41:25 -0800

    Fixed type on build.bat

--- build.bat
+++ build.bat
@@ -17,11 +17,11 @@ if "%nuget%" == "" (
 set nunit=".\NUnit.Console-3.5.0\nunit3-console.exe"
 
 dotnet restore .\Espalier.Validate\project.json
-dotnet build .\Espalier.Validate\project.json --configuration config
+dotnet build .\Espalier.Validate\project.json --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 dotnet restore .\Espalier.Validate.Tests\project.json
-dotnet build .\Espalier.Validate.Tests\project.json --configuration config
+dotnet build .\Espalier.Validate.Tests\project.json --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 %nunit% .\Espalier.Validate.Tests\bin\%config%\Espalier.Validate.Tests.dll
